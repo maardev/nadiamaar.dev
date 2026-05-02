4cdb278784007ca96489f22b3bdb6accb2030b56
Nadiia Marchenko <nadiiamarchenko@iMac-Nadiia.local>
2026-05-02 18:40:47 +0200

    Настроил Header и Mega Menu в Payload

--- nadiamaar-portfolio/src/Footer/RowLabel.tsx
+++ nadiamaar-portfolio/src/Footer/RowLabel.tsx
@@ -6,7 +6,7 @@ export const RowLabel: React.FC<RowLabelProps> = () => {
   const data = useRowLabel<NonNullable<Header['navItems']>[number]>()
 
   // @ts-ignore
-const label = data?.data?.label  data?.data?.link?.label  'Row';
+const label = data?.data?.label || data?.data?.link?.label || 'Row';
     ? `Nav item ${data.rowNumber !== undefined ? data.rowNumber + 1 : ''}: ${data?.data?.link?.label}`
     : 'Row'
 
